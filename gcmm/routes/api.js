01c6ceb0c7db38823ec7b8d12e6fa3fe022a4f99
jihazard <yoonjh238@gmail.com>
2020-03-30 17:58:47 +0900

    update

--- gcmm/routes/api.js
+++ gcmm/routes/api.js
@@ -2,22 +2,25 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host:"192.168.0.27",
-  port:3306,
+  host:"yoonjh238.iptime.org",
+  port:63306,
   user : "root",
   password : "test",
   database :"test"
 })
 
+connection.connect();
 
 /* GET users listing. */
 router.get('/v1/:keyword', function(req, res, next) {
-    console.log(" movie / get ")
+    console.log(req.params.keyword)
+    var keyword = req.params.keyword;
   let obj = {};
-  let query = connection.query("select * from COMMIT_CODE",function(err,rows){
+  let query = connection.query("select * from COMMIT_CODE where CODE= " + mysql.escape(keyword) ,function(err,rows){
       if(err) throw err;
           if(rows.length){
             console.log("result ==> " , rows)
+          
           }else{
               obj.result="0"
           }
@@ -25,7 +28,25 @@ router.get('/v1/:keyword', function(req, res, next) {
          
           // res.json(obj)
       })       
- // res.send('respond with a resource' );
+
+});
+
+
+router.get('/v2/:keyword', function(req, res, next) {
+  console.log(req.params.keyword)
+  var keyword = req.params.keyword;
+let obj = {};
+let query = connection.query("SELECT * FROM COMMIT_CODE TAB1 LEFT OUTER JOIN COMMIT_EXAMPLE TAB2 ON TAB1.CODE = TAB2.CODE WHERE TAB1.CODE = " + mysql.escape(keyword) ,function(err,rows){
+    if(err) throw err;
+        if(rows.length){
+          return res.send(rows);
+        }else{
+          return res.send(obj)
+        }
+        
+        // res.json(obj)
+    })       
+
 });
 
 module.exports = router;
